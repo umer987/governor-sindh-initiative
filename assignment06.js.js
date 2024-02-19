cb093b12512fd3aae858f47ae89932830109e84b
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 11:07:16 +0500

    Add files via upload

---
+++ assignment06.js.js
@@ -0,0 +1,4 @@
+var personNameWithWhitespace = "\t  John \tDoe ";
+console.log("Original Name:", personNameWithWhitespace);
+var strippedName = personNameWithWhitespace.trim();
+console.log("Stripped Name:", strippedName);

e3a8cbe7e27c83a908eec415cf08d05ff0c77404
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 11:15:21 +0500

    Add files via upload

---
+++ assignment07.js
@@ -0,0 +1,8 @@
+var add = 4 + 4;
+console.log("additions is " + add);
+var minus = 16 - 8;
+console.log("subtraction is  " + minus);
+var div = 16 / 2;
+console.log("division is " + div);
+var multi = 2 * 4;
+console.log("multiplication is " + multi);

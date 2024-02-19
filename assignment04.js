c3bd2eaf428267a87eded61021d872dbfcdfe304
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 10:56:46 +0500

    Add files via upload

---
+++ assignment04.js
@@ -0,0 +1,4 @@
+// var famousQuote = "The only limit to our realization of tomorrow will be our doubts of today.";
+// var author = "Franklin D. Roosevelt";
+// console.log(" \" ".concat(famousQuote, " \" - ").concat(author));
+console.log('Albert Einstein once said, "A person who never made a mistake never tried anything new."');

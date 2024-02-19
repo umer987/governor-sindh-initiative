b3d820a854fb4ea2f0cae6811045cad7d6314864
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 10:30:32 +0500

    Add files via upload

---
+++ assignment04.js
@@ -0,0 +1,3 @@
+var famousQuote = "The only limit to our realization of tomorrow will be our doubts of today.";
+var author = "Franklin D. Roosevelt";
+console.log(" \" ".concat(famousQuote, " \" - ").concat(author));

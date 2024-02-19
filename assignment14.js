3a390ee431d674ca3573fd2e9a5bc0f9b4ed839d
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 11:59:53 +0500

    Add files via upload

---
+++ assignment14.js
@@ -0,0 +1,4 @@
+var guestList = ["umer", "ahad", "anas", "zain", "bilal"];
+for (var i = 0; i < guestList.length; i++) {
+    console.log("Dear ".concat(guestList[i], ", you are cordially invited to dinner. It would be an honor to have you join us."));
+}

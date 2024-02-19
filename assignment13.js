578af720148a1a45c5d68419f0619a77e9248616
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 11:49:45 +0500

    Add files via upload

---
+++ assignment13.js
@@ -0,0 +1,4 @@
+var favoriteTransportation = ["Tesla Model S", "Honda Motorcycle", "Vespa Scooter", "Boeing 747"];
+for (var i = 0; i < favoriteTransportation.length; i++) {
+    console.log("I would like to own a ".concat(favoriteTransportation[i], "."));
+}

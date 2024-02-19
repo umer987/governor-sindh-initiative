e36a2c540fcc61fa90c79bffd2d06ab8798084cb
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 00:52:19 +0500

    Add files via upload

---
+++ asssignment20.js
@@ -0,0 +1,7 @@
+// Create a list of favorite fruits
+var favoriteFruits = ["Apple", "Banana", "Orange", "Strawberry", "Mango"];
+// Print each fruit in the list
+console.log("List of Favorite Fruits:");
+for (var i = 0; i < favoriteFruits.length; i++) {
+    console.log(favoriteFruits[i]);
+}

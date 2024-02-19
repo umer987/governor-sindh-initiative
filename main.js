025677e9b7e65124b9876ee14ecf2de830acb7a3
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-19 10:59:04 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-var famousQuote = "The only limit to our realization of tomorrow will be our doubts of today.";
-var author = "Franklin D. Roosevelt";
-console.log(" \" ".concat(famousQuote, " \" - ").concat(author));
+var message = "The only limit to our realization of tomorrow will be our doubts of today.";
+var famous_person = "Franklin D. Roosevelt";
+console.log(" \" ".concat(message, " \" - ").concat(famous_person));

41ee1eb9809116f70beba0edf319da481adcde11
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:54:04 +0500

    Update assignment13.js

--- assignment13.js
+++ assignment13.js
@@ -1,3 +1,6 @@
+Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+
 var favoriteTransportation = ["Tesla Model S", "Honda Motorcycle", "Vespa Scooter", "Boeing 747"];
 for (var i = 0; i < favoriteTransportation.length; i++) {
     console.log("I would like to own a ".concat(favoriteTransportation[i], "."));

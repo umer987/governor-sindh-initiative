f126f965675856cd6a0549ab422f4d3d3b1187a7
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 18:04:20 +0500

    Update assignment26.js

--- assignment26.js
+++ assignment26.js
@@ -1,11 +1,12 @@
-//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
-//• If the alien is green, print a message that the player earned 5 points.
-//
-//• If the alien is yellow, print a message that the player earned 10 points.
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+//• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
 
-//• If the alien is red, print a message that the player earned 15 points.
+//• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
 
-//• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+//• Write one version of this program that runs the if block and another that runs the else block.
+
+
+
 
 
 var alien_color = 'red';

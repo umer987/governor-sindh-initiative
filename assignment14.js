b5ef7f30e8c2381f4c1c8d4cab10596364f96f2f
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:54:28 +0500

    Update assignment14.js

--- assignment14.js
+++ assignment14.js
@@ -1,3 +1,5 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
 var guestList = ["umer", "ahad", "anas", "zain", "bilal"];
 for (var i = 0; i < guestList.length; i++) {
     console.log("Dear ".concat(guestList[i], ", you are cordially invited to dinner. It would be an honor to have you join us."));

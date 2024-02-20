22b5f254990de0ad37d53d771be2b029b343e488
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:52:49 +0500

    Update assignment10.js

--- assignment10.js
+++ assignment10.js
@@ -1,3 +1,6 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
 //printing my name
 console.log("SYED UMER");
 //printing date

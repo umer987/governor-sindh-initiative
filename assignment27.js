921fc00c56e03bda208ba10ff31ba306d7a5eb94
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 10:44:40 +0500

    Add files via upload

---
+++ assignment27.js
@@ -0,0 +1,10 @@
+var alien_color = 'green'; // Change color to yellow or red
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}

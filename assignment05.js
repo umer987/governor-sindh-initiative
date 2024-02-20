e9c35a936b0c1894b20c275baf96504b4be7a580
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:50:37 +0500

    Update assignment05.js

--- assignment05.js
+++ assignment05.js
@@ -1,3 +1,7 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+
 var message = "The only limit to our realization of tomorrow will be our doubts of today.";
 var famous_person = "Franklin D. Roosevelt";
 console.log(" \" ".concat(message, " \" - ").concat(famous_person));

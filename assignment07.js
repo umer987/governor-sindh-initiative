70aed4c98469a75a0e87ee62303291fb9f6a3bc2
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:51:26 +0500

    Update assignment07.js

--- assignment07.js
+++ assignment07.js
@@ -1,3 +1,8 @@
+///Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
+
+
 var add = 4 + 4;
 console.log("additions is " + add);
 var minus = 16 - 8;

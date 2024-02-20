f1b5d72c217a15c767c9382c4435bdd65e7ce51a
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 11:20:04 +0500

    Add files via upload

---
+++ assignment39.js
@@ -0,0 +1,9 @@
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+var l1 = city_country('Lahore', 'Pakistan');
+var l2 = city_country('karachi', 'pakistan');
+var l3 = city_country('islamabad', 'pakistan');
+console.log(l1);
+console.log(l2);
+console.log(l3);

4c94f9f8e88d47fe21ee3920860f47d6a4c112fd
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 18:12:35 +0500

    Update assignment37.js

--- assignment37.js
+++ assignment37.js
@@ -1,3 +1,4 @@
+//Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 function make_shirt(size, message) {
     if (size === void 0) { size = 'large'; }
     if (message === void 0) { message = 'it is shirt'; }

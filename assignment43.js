8bf027ebfe325af2e629d8eb31335b9534744ded
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:27:56 +0500

    Add files via upload

---
+++ assignment43.js
@@ -0,0 +1,29 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
+    }
+}
+function make_great(magicians) {
+    var great_magicians = [];
+    for (var _i = 0, magicians_2 = magicians; _i < magicians_2.length; _i++) {
+        var magician = magicians_2[_i];
+        great_magicians.push("".concat(magician, " the Great"));
+    }
+    return great_magicians;
+}
+var magician_names = ['umer', 'ahad', 'bilal', 'zain'];
+var great_magicians = make_great(__spreadArray([], magician_names, true));
+console.log("Original Magicians:");
+show_magicians(magician_names);
+console.log("\nGreat Magicians:");
+show_magicians(great_magicians);

97d2493cc48f9bb81b2539771a16a759b03489dc
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:47:50 +0500

    Update assignment03.js

--- assignment03.js
+++ assignment03.js
@@ -1,3 +1,8 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+
+
+
 var personName = "Eric";
 console.log("Lowercase:", personName.toLowerCase());
 console.log("Uppercase:", personName.toUpperCase());

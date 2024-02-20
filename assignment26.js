06538efc4f02092343170ace9946bdc38bd7e31f
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 10:41:25 +0500

    Add files via upload

---
+++ assignment26.js
@@ -0,0 +1,7 @@
+var alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Nice shot! You just earned 10 points.");
+}

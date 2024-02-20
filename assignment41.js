93f0b6f7e14416b8a228cf7ca710b702b54c2bd6
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:18:43 +0500

    Add files via upload

---
+++ assignment41.js
@@ -0,0 +1,8 @@
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
+    }
+}
+var magician_names = ['umer', 'ahad', 'bilal', 'zain'];
+show_magicians(magician_names);

1b6670f840abe7ba7c5266ebf9ed065d7f6c7b09
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:47:18 +0500

    Update assignment 02.js

--- assignment 02.js
+++ assignment 02.js
@@ -1,2 +1,6 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+
 var pername = "eric";
 console.log("hello " + pername + "would you like to learn python today");

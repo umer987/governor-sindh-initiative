0c395be95aae21178e9ace4c945cf3f91c13e4de
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:52:26 +0500

    Update assignment09.js

--- assignment09.js
+++ assignment09.js
@@ -1,3 +1,6 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
 var favnum = 1;
 var mess = 'my favaroute number is"${favnum}" ';
 console.log(mess);

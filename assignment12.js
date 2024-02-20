6fc4103729c765839b147a0721020c0bcb796694
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:53:46 +0500

    Update assignment12.js

--- assignment12.js
+++ assignment12.js
@@ -1,3 +1,4 @@
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
 var friends = ["umer", "ahad", "anas", "zain", "bilal"];
 for (var i = 0; i < friends.length; i++) {
     console.log('hello, ${friends[i]} have a nice party');

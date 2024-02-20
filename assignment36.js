1d51270acf51f1952d0660f0d56347fbda39129d
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 11:10:30 +0500

    Add files via upload

---
+++ assignment36.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) {
+    console.log("Shirt Size: ".concat(size.toUpperCase()));
+    console.log("Message: ".concat(message));
+}
+make_shirt('medium', 'it is shirt');

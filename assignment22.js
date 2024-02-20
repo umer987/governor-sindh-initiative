9d6f9317f51cf31c456b21344017688c3a94ccec
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:59:53 +0500

    Update assignment22.js

--- assignment22.js
+++ assignment22.js
@@ -1,3 +1,4 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
 // Create an array of favorite fruit objects
 var favoriteFruits = [
     { name: "Apple", color: "Red", taste: "Sweet" },

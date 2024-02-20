eb1ba9b39b5a6c1763b546ede8b6b0244390868e
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 18:15:04 +0500

    Update assignment42.js

--- assignment42.js
+++ assignment42.js
@@ -1,3 +1,4 @@
+//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 function show_magicians(magicians) {
     for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
         var magician = magicians_1[_i];

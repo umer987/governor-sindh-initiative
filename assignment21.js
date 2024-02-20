bc594b71ef91112eb6914ef921559067598778da
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 10:05:47 +0500

    Add files via upload

--- assignment21.js
+++ assignment21.js
@@ -1,11 +1,18 @@
-var apple = { name: "Apple" };
-var banana = { name: "Banana" };
-var orange = { name: "Orange" };
-var strawberry = { name: "Strawberry" };
-var mango = { name: "Mango" };
-var favoriteFruits = [apple, banana, orange, strawberry, mango];
-console.log("Details about Favorite Fruits:");
-for (var i = 0; i < favoriteFruits.length; i++) {
-    var fruit = favoriteFruits[i];
-    console.log("".concat(fruit.name));
+// Create an array of favorite fruit objects
+var favoriteFruits = [
+    { name: "Apple", color: "Red", taste: "Sweet" },
+    { name: "Banana", color: "Yellow", taste: "Creamy" },
+    { name: "Orange", color: "Orange", taste: "Citrusy" },
+    { name: "Strawberry", color: "Red", taste: "Juicy" },
+    { name: "Mango", color: "Yellow", taste: "Sweet and Tangy" }
+];
+// Try to access an element at an invalid index (intentional error)
+var invalidIndex = 10; // using an index that doesn't exist
+var fruit = favoriteFruits[invalidIndex];
+// Correct the error by checking if the index is valid
+if (fruit) {
+    console.log("Details about Favorite Fruit at index ".concat(invalidIndex, ": ").concat(fruit.name, ", Color - ").concat(fruit.color, ", Taste - ").concat(fruit.taste));
+}
+else {
+    console.log("Error: Invalid index ".concat(invalidIndex, ". Please choose a valid index."));
 }

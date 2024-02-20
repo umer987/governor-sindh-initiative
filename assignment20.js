85651df43756a1cded7b55af83cba2c623149755
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:58:10 +0500

    Update assignment20.js

--- assignment20.js
+++ assignment20.js
@@ -1,3 +1,6 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
 // Create a list of favorite fruits
 var favoriteFruits = ["Apple", "Banana", "Orange", "Strawberry", "Mango"];
 // Print each fruit in the list

7fb9d1e25c4f2200b366b24cc9f1b7fe93393163
umer987 <159330908+umer987@users.noreply.github.com>
2024-02-20 17:57:42 +0500

    Update assignment19.js

--- assignment19.js
+++ assignment19.js
@@ -1,2 +1,3 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
 var guestList = ["umer", "ahad", "anas", "zain", "bilal"];
 console.log("here is the number of gest", guestList.length);

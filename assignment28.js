8ba64f5fb5d142040659b7c627d48b424ef13fd8
umer987 <159330908+umer987@users.noreply.github.com>
2025-02-22 00:14:46 +0500

    Update assignment28.js

--- assignment28.js
+++ assignment28.js
@@ -9,7 +9,7 @@
 
 //• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 
-//• If the person is age 65 or older, print a message that the person is an elder.
+
 
 
 

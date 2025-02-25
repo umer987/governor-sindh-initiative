0e1e881c4299712b538052098be1d7652706f05d
umer987 <159330908+umer987@users.noreply.github.com>
2025-02-25 23:04:45 +0500

    Update assignment43.js

--- assignment43.js
+++ assignment43.js
@@ -1,4 +1,4 @@
-//Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+/ed, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {

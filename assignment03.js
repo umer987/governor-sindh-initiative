b3e043ce1568ed73c10870fc43c9703a1a658e34
umer987 <159330908+umer987@users.noreply.github.com>
2025-03-08 23:13:04 +0500

    Update assignment03.js

--- assignment03.js
+++ assignment03.js
@@ -1,4 +1,4 @@
-//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+le, and then print that person’s name in lowercase, uppercase, and titlecase.
 
 
 
